1033b0a5ad3098fec4754aebf0ee50ad6b17abea
Chris <who7708@163.com>
2021-04-30 00:57:05 +0800

    init

---
+++ src/test/kotlin/org/example/demo/DemoApplicationTests.kt
@@ -0,0 +1,14 @@
+package org.example.demo
+
+import org.junit.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        println("test")
+    }
+
+}
