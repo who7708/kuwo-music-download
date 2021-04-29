f3b2b1bb5036b248b97b2da22404559eb84a2ea7
Chris <who7708@163.com>
2021-04-30 01:00:54 +0800

    junit test

--- src/test/kotlin/org/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/org/example/demo/DemoApplicationTests.kt
@@ -1,14 +1,25 @@
 package org.example.demo
 
+// import org.junit.jupiter.api.Test
+// import org.junit.runner.RunWith
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.junit4.SpringRunner
 
 @SpringBootTest
+@RunWith(SpringRunner::class)
 class DemoApplicationTests {
+
+    @Autowired
+    private lateinit var ctx: ApplicationContext
 
     @Test
     fun contextLoads() {
         println("test")
+        println(ctx.applicationName)
     }
 
 }
